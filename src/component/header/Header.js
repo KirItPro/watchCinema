acbd207614a62620b8b4707f2495438e3b51a13c
KirItPro <117309466+KirItPro@users.noreply.github.com>
2024-03-19 17:33:59 +0300

    Added promo video

--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -9,7 +9,6 @@ export default function Header() {
             <div className="header-menu">
             <Link to='/' className="header-menu_button">Главная</Link>
             <Link to='/all-films' className="header-menu_button">Фильмы</Link>
-            <Link to='/' className="header-menu_button">Сериалы</Link>
             <Link to='/favorites' className="header-menu_button">Избранное(<div className='header-menu_button__count'>{items.length}</div>)</Link>
             </div>
         </header>
--- src/component/promo/Promo.js
+++ src/component/promo/Promo.js
@@ -1,10 +1,23 @@
+import ReactPlayer from 'react-player';
 import './promo.css';
-import BG_VIDEO from '../../video/bg-video.mp4';
+// import BG_VIDEO from '../../video/bg-video.mp4';
+
+const TRAILER = 'https://www.youtube.com/watch?v=sBU8ejvW6fM';
 
 export default function Promo() {    
     return (
         <div className="promo" >
-            <video src={BG_VIDEO} autoPlay muted loop className='promo-video'></video>
+            {/* <video src={BG_VIDEO} autoPlay muted loop className='promo-video'></video> */}
+            <ReactPlayer
+                className="promo-video"
+                controls={true}
+                width='98vw'
+                height='120%'
+                loop={true}
+                muted={true}
+                playing={true}
+                url={TRAILER} 
+            />
             <h1 className="promo-title">смотри кино</h1>
         </div>
     )
